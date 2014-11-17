a8e2ee9d10ed380d25d41be37c2f51d85d7792a5
Justin Rogers <yaskyj@yahoo.com>
2014-11-17 18:58:44 +0000

    Add other characters and gem images

--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,15 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png'
     ]);
     Resources.onReady(init);
 
