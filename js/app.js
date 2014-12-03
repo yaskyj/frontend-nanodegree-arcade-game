0636c0ae95e923a848e8ad17a6ae8663e5be19e1
Justin Rogers <yaskyj@yahoo.com>
2014-12-02 20:01:15 -0600

    Add proper semicolons to functions and if statements

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var Enemy = function() {
 
     //array holds the three possible y values for enemies
     this.yValues = [220, 137, 54];
-}
+};
 
 //initializes enemies
 Enemy.prototype.init = function () {
@@ -15,7 +15,7 @@ Enemy.prototype.init = function () {
     this.y = this.yValues[Math.floor(Math.random() * this.yValues.length)];
     //randomly sets a velocity between 200 & 300
     this.velocity = Math.floor((Math.random() * 300) + 200);
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -25,26 +25,26 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += (this.velocity * dt);
     this.checkOutOfBounds();
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //checks for enemy leaving the screen and reinitializes
 Enemy.prototype.checkOutOfBounds = function() {
     if (this.x > 505) {
         this.init();
-    };
-}
+    }
+};
 
 //class for the gem treasures
 var Treasure = function() {
     this.sprites = ['images/Gem Blue.png', 'images/Gem Green.png', 'images/Gem Orange.png'];
     this.yValues = [220, 137, 54];
     this.xValues = [0, 101, 202, 303, 404];
-}
+};
 
 //initializes the treasures with a random sprite and random 
 Treasure.prototype.init = function() {
@@ -52,37 +52,37 @@ Treasure.prototype.init = function() {
     this.x = this.xValues[Math.floor(Math.random() * this.xValues.length)];
     this.y = this.yValues[Math.floor(Math.random() * this.yValues.length)];
 
-}
+};
 
 //render gem
 Treasure.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //player class
 var Player = function() {
     //array to hold all player sprites
     this.sprites = ['images/char-boy.png', 'images/char-cat-girl.png', 'images/char-horn-girl.png', 'images/char-pink-girl.png', 'images/char-princess-girl.png']
-}
+};
 
 //initializes the player with a random character sprite, sets to origin point, and sets score to zero
 Player.prototype.init = function() {
     this.sprite = this.sprites[Math.floor(Math.random() * this.sprites.length)];
     this.setToOrigin();
     this.score = 0;
-}
+};
 
 //player update function
 Player.prototype.update = function() {
     this.checkOutOfBounds();
     this.collisionDetection(allEnemies, treasure);
     this.increaseScore();
-}
+};
 
 //player render
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //checks player collisions with enemies and gems
 Player.prototype.collisionDetection = function(enemies, treasure) {
@@ -90,69 +90,69 @@ Player.prototype.collisionDetection = function(enemies, treasure) {
         //if player collides with enemies it is reinitialized
         if (enemies[enemy].y == this.y && (Math.abs(enemies[enemy].x - this.x) <= 60)) {
             this.init();
-        };
+        }
     }
 
     //checks collision with the treasure and adds 10000 to score
     if (treasure.y == this.y && treasure.x == this.x) {
         this.score += 10000;
         treasure.init();
-    };
+    }
 
-}
+};
 
 //sets the player back to the origin point
 Player.prototype.setToOrigin = function() {
     this.x = 202;
     this.y = 386;   
-}
+};
 
 //forces the player sprite back into the screen if it's outside and resets to the origin point if
 //it is in the water
 Player.prototype.checkOutOfBounds = function() {
     if (this.x < 0) {
         this.x = 0;
-    };
+    }
 
     if (this.x > 404) {
         this.x = 404;
-    };
+    }
 
     if (this.y < 54) {
         this.setToOrigin();
-    };
+    }
 
     if (this.y > 386) {
         this.y = 386;
-    };
+    }
 
-}
+};
 
 //handles directional input for the player
 Player.prototype.handleInput = function(input) {
     if (input === 'left') {
         this.x -= 101;
-    };
+    }
 
     if (input === 'right') {
         this.x += 101;
-    };
+    }
 
     if (input === 'up') {
         this.y -= 83;
-    };
+    }
 
     if (input === 'down') {
         this.y += 83;
-    };
-}
+    }
+};
 
 //if the player is on the stones then the score is increased
 Player.prototype.increaseScore = function(dt) {
     if (this.y <= 220 && this.y >= 54) {
         this.score += 1;
-    };
-}
+    }
+};
 
 //takes a number and creates a corresponding number of enemies
 function createEnemies(numEnemies) {
